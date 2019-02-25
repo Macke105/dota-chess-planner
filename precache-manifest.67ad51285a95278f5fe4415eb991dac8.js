0ae8f11426833f115d05cd3786e8e0388ff2869e
Marcus Jonsson <marcus.jonsson105@gmail.com>
2019-02-25 23:05:23 +0100

    Updates

---
+++ precache-manifest.67ad51285a95278f5fe4415eb991dac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dota-chess-planner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ac66059165ddc706439a",
+    "url": "/dota-chess-planner/static/js/runtime~main.ac660591.js"
+  },
+  {
+    "revision": "97f9ebd2c0e5e3766dd0",
+    "url": "/dota-chess-planner/static/js/main.97f9ebd2.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/dota-chess-planner/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "97f9ebd2c0e5e3766dd0",
+    "url": "/dota-chess-planner/static/css/main.78eaeab9.chunk.css"
+  },
+  {
+    "revision": "1d9a1508927584632216c09fa747d7d5",
+    "url": "/dota-chess-planner/index.html"
+  }
+];
